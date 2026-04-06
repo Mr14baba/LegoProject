f98f0c90c6ec23cf2617dba568061d3b3b094ee5
Mr14baba <161134895+Mr14baba@users.noreply.github.com>
2026-04-06 13:22:06 +0200

    Update DropdownWithImage.cs

--- Assets/Scripts/UI/DropdownWithImage.cs
+++ Assets/Scripts/UI/DropdownWithImage.cs
@@ -1,8 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 [UxmlElement]
 public partial class DropdownWithImage : BaseField<string>
 {
+    public class Item
+    {
+        public string iconLabel;
+        public Texture2D iconTexture;
+    }
+
+    private List<Item> itemsList = new();
+    private VisualElement popup;
+    private Label selectedIconLabel;
+    private Image selectedIconTexture;
+    private bool isOpen;
+    
     public DropdownWithImage() : this(null)
     {
         
@@ -10,7 +25,14 @@ public partial class DropdownWithImage : BaseField<string>
 
     public DropdownWithImage(string label) : base(label, new VisualElement())
     {
-        
+        AddToClassList("image-dropdown-field");
+        var display = this.Q<VisualElement>(className: "unity-base-field__input");
+        display.style.flexDirection = FlexDirection.Row;
+        display.style.alignItems = Align.Center;
+        display.style.paddingLeft = 6;
+        display.style.paddingRight = 6;
+
+        selectedIconLabel = new Label("Red") {style = {width = 20, height = 20, marginRight = 6}};
     }
 
 }
